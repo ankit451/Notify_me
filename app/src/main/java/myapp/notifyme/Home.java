417eaf1e9f8fbc59f12714e721010b469d9c29e7
Manjur Ansari <Manjur Ansari>
2016-03-05 20:43:08 +0530

    all working updated

--- app/src/main/java/myapp/notifyme/Home.java
+++ app/src/main/java/myapp/notifyme/Home.java
@@ -9,6 +9,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
