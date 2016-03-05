5c6631793a74a264fbe23a8bfe8adf281e6cff03
Manjur Ansari <Manjur Ansari>
2016-03-05 19:45:31 +0530

    Updated background of activities

--- app/src/main/java/myapp/notifyme/groups.java
+++ app/src/main/java/myapp/notifyme/groups.java
@@ -1,7 +1,10 @@
 package myapp.notifyme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class groups extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class groups extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groups);
+
+        TextView cnt = (TextView)findViewById(R.id.textView2);
+        cnt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(groups.this, classes.class));
+            }
+        });
+
+        TextView cnt1 = (TextView)findViewById(R.id.textView3);
+        cnt.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(groups.this, classes.class));
+            }
+        });
     }
 }
